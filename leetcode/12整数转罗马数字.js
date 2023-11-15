e58d4bd5e88d514ff5cf049bacf895c2601d5014
陈子进 <wsczj@foxmail.com>
2023-11-15 23:59:01 +0800

    leetcode12

---
+++ leetcode/12整数转罗马数字.js
@@ -0,0 +1,27 @@
+var intToRoman = function(num) {
+    let ans = []
+    let x = new Map()
+    x.set(1000, "M")
+    x.set(900, "CM")
+    x.set(500, "D")
+    x.set(400, "CD")
+    x.set(100, "C")
+    x.set(90, "XC")
+    x.set(50, "L")
+    x.set(40, "XL")
+    x.set(10, "X")
+    x.set(9, "IX")
+    x.set(5, "V")
+    x.set(4, "IV")
+    x.set(1, "I")
+    for([key,value] of x){
+        while(num>=key){
+            num = num-key
+            ans.push(value)
+        }
+        if(num==0){
+            break;
+        }
+    }
+    return ans.join('')
+};
