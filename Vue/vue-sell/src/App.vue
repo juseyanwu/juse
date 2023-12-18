101030669afd1baa66f0a34bbd33112ceabac3cc
陈子进 <wsczj@foxmail.com>
2023-12-18 23:57:30 +0800

    12.18

---
+++ Vue/vue-sell/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-header />
+  <router-view/>
+</template>
+
+<script>
+import Header from '@/components/header/Header.vue'
+import axios from 'axios'
+export default{
+  components:{
+    'v-header':Header
+  },
+  created(){
+    axios.get('/api/seller')
+    .then((data)=>{
+      console.log(data);
+    })
+  }
+
+}
+</script>
+
+
+
+
+<style lang="less">
+
+</style>
---
+++ Vue/vue-sell/src/components/header/Header.vue
@@ -0,0 +1,127 @@
+<template>
+    <div class="header">
+        <div class="content-wrapper">
+            <div class="avatar">
+                <img src="http://static.galileo.xiaojukeji.com/static/tms/seller_avatar_256px.jpg" alt="">
+            </div>
+            <div class="content">
+                <div class="title">
+                    <span class="brand"></span>
+                    <span class="name">店铺名称</span>
+                </div>
+                <div class="description">蜂鸟专送/38分钟送达</div>
+                <div class="support">
+                    <!-- xxx -->
+                    <SupportIcon :size="1" :type="0"/>
+                    <span class="text">在线支付满28减5</span>
+                </div>
+            </div>
+            <div class="support-count">
+                <span class="count">5个</span>
+                <i class="iconfont icon-right"></i>
+            </div>
+        </div>
+        <div class="bulletin-wrapper">
+
+        </div>
+        <div class="bg" style="background-image: url('http://static.galileo.xiaojukeji.com/static/tms/seller_avatar_256px.jpg');"></div>
+    </div>
+</template>
+
+<script>
+import SupportIcon from '@/components/support-icon/Support-icon.vue'
+    export default {
+        components:{
+            SupportIcon
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+@import '@/common/style/variable.less';
+@import '@/common/style/mixin.less';
+
+.header{
+    position: relative;
+    background:@color-background-ss;
+    .content-wrapper{
+        position: relative;
+        display: flex;
+        padding: 24px 12px 18px 24px;
+        .avatar{
+            width: 64px;
+            height: 64px;
+            img{
+                width: 100%;
+                border-radius: 2px;
+            }
+        }
+        .content{
+            color:@color-white;
+            flex: 1;
+            margin-left: 16px;
+            .title{
+                margin-bottom: 8px;
+                display: flex;
+                align-items: center;
+                .brand{
+                    width: 30px;
+                    height: 18px;
+                    .bg-image('brand');
+                    background-size: cover;
+                }
+                .name{
+                    margin-left: 6px;
+                    font-size: @fontsize-large;
+                    font-weight: bold;
+                }
+            }
+            .description{
+                font-size:@fontsize-small;
+                margin-bottom: 8px;
+            }
+            .support{
+                display: flex;
+                align-items: center;
+                .text{
+                    font-size:@fontsize-small-s;
+                    margin-left: 4px;
+                }
+            }
+        }
+        .support-count{
+            position: absolute;
+            right: 12px;
+            bottom: 14px;
+            padding: 0 8px;
+            height: 24px;
+            text-align: center;
+            background: @color-background-sss;
+            border-radius: 14px;
+            display: flex;
+            align-items: center;
+            color:@color-white;
+            .count{
+                font-size:@fontsize-small-s ;
+            }
+            .iconfont{
+                font-size:@fontsize-small-s ;
+                margin-left: 2px;
+            }
+        }
+    }
+
+    .bg{
+        position: absolute;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+        background-size: 100% 100%;
+        z-index: -1;
+        filter: blur(10px);
+    }
+}
+
+
+</style>
