593c334a127676254934ae3a2103e629e1fbd721
陈子进 <wsczj@foxmail.com>
2024-01-01 23:45:04 +0800

    1.1

--- leetcode/2.js
+++ leetcode/2.js
@@ -1,16 +1,11 @@
-var numIdenticalPairs = function(nums) {
-    if(nums.length == 1){
-        return 0;
-    }
-    const bucket = Array(101).fill(0);
-    for(let i = 0; i < nums.length; i++){
-        bucket[nums[i]]++;
-    }
-    let res = 0;
-    for(let i = 1; i < bucket.length; i++){
-        if(bucket[i]){
-            res += bucket[i] * (bucket[i] - 1)  / 2;
+var longestCommonPrefix = function(strs) {
+    var re = '';
+    if (!strs.length) return re;
+    for (var j=0;j<strs[0].length;j++){//第j位
+        for (var i=1;i<strs.length;i++){//第i个
+            if (strs[i][j]!=strs[0][j]) return re
         }
+        re += strs[0][j];
     }
-    return res;
+    return re;
 };
