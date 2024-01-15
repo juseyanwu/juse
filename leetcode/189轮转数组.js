f83f1b27332ad5cec4184c4f1493de9b7f97dc12
陈子进 <wsczj@foxmail.com>
2024-01-15 19:51:33 +0800

    1.15

---
+++ leetcode/189轮转数组.js
@@ -0,0 +1,12 @@
+let reverse = function(nums, start, end){
+    while(start < end){
+        [nums[start++], nums[end--]] = [nums[end], nums[start]];
+    }
+}
+let rotate = function(nums, k) {
+    k %= nums.length;
+    reverse(nums, 0, nums.length - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, nums.length - 1);
+    return nums;
+};
