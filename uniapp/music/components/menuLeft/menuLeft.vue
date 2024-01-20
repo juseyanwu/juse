76fc21f518a4da015e34cbbecf6fc3cd50fe1d11
陈子进 <wsczj@foxmail.com>
2024-01-21 04:31:31 +0800

    uniapp-music

---
+++ uniapp/music/components/menuLeft/menuLeft.vue
@@ -0,0 +1,90 @@
+<template>
+	<view class="menu-left" :class="isShow ? 'show' : 'hide'">
+		<view class="menu-hd">
+			<view class="user-info">
+				<view class="avatar">
+					<img src="../../static/icon/wangyiyun.png" mode="aspectFill" class="pic" />
+				</view>
+				<view class="username">
+					<text>陈子进</text>
+					<uni-icons type="right" size="16"></uni-icons>
+				</view>
+			</view> 
+			<view class="qricon">
+				<uni-icons type="scan" size="25"></uni-icons>
+			</view>
+		</view>
+		
+		<view class="menu-bd">
+			hello
+		</view>
+	</view>
+	
+	<view class="menu-mask" @click="hideMenu" v-show="isShow"></view>
+</template>
+
+<script setup>
+import { computed } from 'vue';
+import {useStore} from 'vuex'
+
+const store = useStore()
+
+
+const isShow = computed(()=>{
+	return store.state.isShowMenu
+})
+const hideMenu = () => {
+	store.commit('changeisShowMenu',false)
+}
+</script>
+
+<style lang="scss" scoped>
+.menu-left{
+	position: fixed;
+	top: 0;
+	z-index: 9999;
+	width: 620rpx;
+	padding: 30rpx;
+	box-sizing: border-box;
+	background-color: #fff;
+	height: 100%;
+	transition: all 0.5s ease-in-out;
+	&.show{
+		left: 0;
+	}
+	&.hide{
+		left: -620rpx;
+	}
+	.menu-hd{
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		.user-info{
+			display: flex;
+			align-items: center;
+			.avatar{
+				width: 80rpx;
+				height: 80rpx;
+				border-radius: 50%;
+				overflow: hidden;
+				.pic{
+					height: 100%;
+					width: 100%;
+				}
+			}
+			.username{
+				margin-left: 20rpx;
+			}
+		}
+	}
+}
+.menu-mask{
+		position: fixed;
+		top: 0;
+		left: 0;
+		bottom: 0;
+		right: 0;
+		background-color: rgba(0, 0, 0, 0.5);
+		z-index: 1000;
+}
+</style>
---
+++ uniapp/music/pages/index/index.vue
@@ -0,0 +1,64 @@
+<template>
+	<view class="index">
+		<wyheader>
+			将内容填充到名为content的插槽中
+			<template v-slot:content>
+				<view class="search">
+					<uni-easyinput style="{height: 60rpx;}" prefixIcon="search" v-model="value" placeholder="请输入内容" @iconClick="iconClick"></uni-easyinput>
+				</view>
+			</template>
+		</wyheader>
+		
+		<menuLeft />
+		
+		
+		<view class="banner">
+			<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" circular="true" indicator-color="#eee" indicator-active-color="#d81e06">
+				<swiper-item v-for="item in state.banners" :key="item.encodeId">
+					<view class="swiper-item">
+						<image :src="item.pic" mode=""></image>
+					</view>
+				</swiper-item>
+			</swiper>
+		</view>
+	</view>
+</template>
+
+<script setup>
+import {apiGetBanner} from '../../api/index.js'
+import {onLoad} from '@dcloudio/uni-app'
+import { reactive } from 'vue';
+
+
+const state = reactive({
+	banners:[]
+})
+
+onLoad(()=>{
+	getBanner()
+})
+
+const getBanner = () => {
+	apiGetBanner({type:2}).then(res=>{
+		console.log(res.data.banners);
+		state.banners = res.data.banners
+	})
+}
+</script>
+
+<style lang="scss" scoped>
+	.index{
+		padding: 0 15rpx;
+	}
+.banner{
+	.swiper-item{
+		border-radius: 10px;
+		height: 100%;
+		overflow: hidden;
+		image{
+			width: 100%;
+			height: 100%;
+		}
+	}
+}
+</style>
