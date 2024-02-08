9e7a4ea6e532414fdad82d359d92cd5f2ee8feed
陈子进 <wsczj@foxmail.com>
2024-02-08 23:02:58 +0800

    ’2.8
    git commit -m ’2.8

--- leetcode/4.js
+++ leetcode/4.js
@@ -1,6 +1,11 @@
-var convertTemperature = function(celsius) {
-    let ans = []
-    ans.push(celsius+273.15)
-    ans.push(celsius*1.80 +32.00)
-    return ans
+var twoSum = function (numbers, target) {
+    let left = 0;
+    let right = numbers.length - 1;
+    while (true) {
+        let s = numbers[left] + numbers[right];
+        if (s === target) {
+            return [left + 1, right + 1]; // 题目要求下标从 1 开始
+        }
+        s > target ? right-- : left++;
+    }
 };
