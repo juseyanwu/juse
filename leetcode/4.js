ecef349296e8bcecb874da048f2c8c9350712bb7
陈子进 <wsczj@foxmail.com>
2024-02-11 21:12:02 +0800

    ’2.11
    git push origin master
    git commit -m ’2.11

--- leetcode/4.js
+++ leetcode/4.js
@@ -1,11 +1,32 @@
-var twoSum = function (numbers, target) {
-    let left = 0;
-    let right = numbers.length - 1;
-    while (true) {
-        let s = numbers[left] + numbers[right];
-        if (s === target) {
-            return [left + 1, right + 1]; // 题目要求下标从 1 开始
-        }
-        s > target ? right-- : left++;
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+var convert = function(s, numRows) {
+    const n = s.length, r = numRows;
+    if (r === 1 || r >= n) {
+        return s;
     }
+    const t = r * 2 - 2;
+    const c = Math.floor((n + t - 1) / t) * (r - 1);
+    const mat = new Array(r).fill(0).map(() => new Array(c).fill(0));
+    for (let i = 0, x = 0, y = 0; i < n; ++i) {
+        mat[x][y] = s[i];
+        if (i % t < r - 1) {
+            ++x; // 向下移动
+        } else {
+            --x;
+            ++y; // 向右上移动
+        }
+    }
+    const ans = [];
+    for (const row of mat) {
+        for (const ch of row) {
+            if (ch !== 0) {
+                ans.push(ch);
+            }
+        }
+    }
+    return ans.join('');
 };
