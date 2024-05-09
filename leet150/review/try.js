732a50682bdf7fb4d9805d0687fa6f82aed5fa2d
陈子进 <wsczj@foxmail.com>
2024-05-09 12:10:24 +0800

    网络

--- leet150/review/try.js
+++ leet150/review/try.js
@@ -18,3 +18,38 @@ function sum (a,b){
 let _sum = curry(sum)
 
 _sum(1)(2)
+
+
+function debounce (fn,delay){
+    let timer
+    return function (){
+        if(timer) clearTimeout(timer)
+        let args = arguments
+        timer = setTimeout(()=>{
+            fn.apply(this,args)
+        },delay)
+    }
+}
+
+function throttle(fn,delay){
+    let pretime = Date.now()
+    return function(){
+        if(Date.now() - pretime > delay){
+            fn.apply(this,arguments)
+            pretime = Date.now()
+        }
+    }
+}
+
+function curry (fn){
+    return function curried(...args){
+        if(args.length >= fn.length){
+            return fn.apply(this,args)
+        }
+        else{
+            return function(...args2){
+                return curried(this,args.concat(args2))
+            }
+        }
+    }
+}
