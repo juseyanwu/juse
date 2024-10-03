b7cf4cd4afb69b35cd403ed34aa919aa07ffb1de
chenzijin <wsczj@foxmail.com>
2024-10-04 00:28:01 +0800

    feat: teach

---
+++ teach/teach-vue/teach-zjy/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+<div class='xxx' :class="abc ? 'abc' : ''" @mouseenter="handleHover">
+  xxx
+</div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const abc = ref(false)
+const handleHover = () => {
+  abc.value = true
+}
+</script>
+
+<style scoped>
+.xxx {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 300px;
+  height: 300px;
+  border-radius: 20%;
+  border: 1px solid red;
+  margin: 20px;
+  padding: 20px;
+}
+.abc {
+  transform: rotateZ(360deg);  
+  transition: 5s;
+}
+</style>
