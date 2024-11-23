a69d50527c8f28545eca66607f0b46b0f324b24a
陈子进 <wsczj@foxmail.com>
2024-11-24 02:44:23 +0800

    feat: test

--- leet150/review/day1/2两数相加.js
+++ leet150/review/day1/2两数相加.js
@@ -59,3 +59,21 @@ function addTwoNumbers(l1,l2){
     if(carry>0) cur.next = new ListNode(carry)
     return head.next 
 }
+
+function addTwoNumbers(l1,l2){
+    let head = new ListNode(0)
+    let cur = head
+    let carry = 0
+    while(l1||l2){
+        let n1 = l1? l1.val : 0
+        let n2 = l2? l2.val : 0
+        let sum = n1 + n2 + carry
+        carry = Math.floor(sum/10)
+        cur.next = new ListNode(sum%10)
+        cur = cur.next
+        l1 = l1?l1.next : null
+        l2 = l2?l2.next : null
+    }
+    if(carry>0) cur.next = new ListNode(carry)
+    return head.next 
+}
